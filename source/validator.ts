f842069cfa7422a692a68bfbd4d686dbcae4352e
Brian Faust <hello@basecode.sh>
2021-09-23 08:41:53 +0300

    refactor: allocate stakes based on voter stakes

--- source/validator.ts
+++ source/validator.ts
@@ -3,5 +3,15 @@ export interface IValidator {
   stake: number;
 }
 
-export const validator = (publicKey: string, stake: number): IValidator =>
-  Object.freeze({ publicKey, stake });
+export interface IVoter {
+  stake: number;
+}
+
+export const validator = (publicKey: string, voters: IVoter[]): IValidator =>
+  Object.freeze({
+    publicKey,
+    stake: voters.reduce(
+      (total: number, voter: IVoter) => total + voter.stake,
+      0,
+    ),
+  });
